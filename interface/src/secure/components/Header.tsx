a32a42d765453a825a62513e6641c76d296281bd
Amar Kanth <amarkanth@Amars-MacBook-Pro.local>
2022-07-07 00:01:05 +0530

    added logout functionality in react

--- interface/src/secure/components/Header.tsx
+++ interface/src/secure/components/Header.tsx
@@ -1,13 +1,43 @@
-const Header = () => (
-	<header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="!#">Company name</a>
+import React, {Component} from "react";
+import axios from 'axios';
+import {Navigate} from 'react-router-dom';
 
-        <div className="navbar-nav">
-            <div className="nav-item text-nowrap">
-                <a className="nav-link px-3" href="!#">Sign out</a>
-            </div>
-        </div>
-    </header>
-);
+
+class Header extends Component {
+    state = {
+        redirect: false
+    }
+
+    handleClick = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        
+        await axios.get('logout/').then(res => {
+            localStorage.removeItem('refresh');
+            axios.defaults.headers.common['Authorization'] = "";
+        });
+
+        this.setState({
+            redirect: true
+        });
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return <Navigate to={"/login"} />
+        }
+
+        return (
+            <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="!#">Company name</a>
+
+                <div className="navbar-nav">
+                    <div className="nav-item text-nowrap">
+                        <a className="nav-link px-3" href="!#" onClick={this.handleClick}>Sign out</a>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
 
 export default Header;
